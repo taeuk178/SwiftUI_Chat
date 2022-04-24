6a556cbf75a77464d50d1221f9229005f2923977
taeuk178 <nous178@naver.com>
2022-04-24 09:00:14 +0900

    Initial Commit

---
+++ SwiftUI_Chat/SwiftUI_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChatApp.swift
+//  SwiftUI_Chat
+//
+//  Created by tw on 2022/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
